6cea149583cd54972ec160a680131301df0b8a66
trini.feng23 <trini.feng23@d155.org>
2023-05-02 08:09:35 -0500

    Building shared preferences for authentication 5/2

--- app/src/main/java/com/example/spotifyplaylistsearch/LoginScreen.kt
+++ app/src/main/java/com/example/spotifyplaylistsearch/LoginScreen.kt
@@ -3,10 +3,15 @@ package com.example.spotifyplaylistsearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
 
 class LoginScreen : AppCompatActivity() {
 
     lateinit var authButton: Button
+    lateinit var requestQueue: RequestQueue
+    val REQUEST_CODE = 1234
+    val SCOPES = "user-read-email,user-read-private,playlist-modify-private,playlist-modify-public"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +19,18 @@ class LoginScreen : AppCompatActivity() {
 
         authButton = findViewById(R.id.auth_button)
 
+        //Initialize shared preferences and volley request queue
+        val sharedPreferences = getSharedPreferences(getString(R.string.shared_pref_key), MODE_PRIVATE)
+        requestQueue = Volley.newRequestQueue(this)
+
+        //Authenticate Spotify app
+
         authButton.setOnClickListener {
+            val myEditor = sharedPreferences.edit()
+
+            myEditor.apply {
+                apply()
+            }
 
         }
     }
